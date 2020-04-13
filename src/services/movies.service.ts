f95d40e220daf23a2a1593c05d4dcaa4168d4a22
David Murillo <dmz866@hotmail.com>
2020-04-12 23:48:24 -0500

    added movie service

--- src/services/movies.service.ts
+++ src/services/movies.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RespuestaMDB } from '../app/interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
+  
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getFeature() {
+    return this.http.get<RespuestaMDB>('https://api.themoviedb.org/3/movie/550?api_key=4208517fc3bbee1ef43597f5e7f87ff9');
+  }
 }
